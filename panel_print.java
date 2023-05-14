c91bf1166cf163a48111df0b46bac05b41e06098
Ajmera23 <86102611+Ajmera23@users.noreply.github.com>
2023-05-14 14:53:35 +0530

    Add files via upload

---
+++ panel_print.java
@@ -0,0 +1,20 @@
+
+import java.awt.Graphics2D;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+class panel_print {
+
+    static void print(Graphics2D g2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
